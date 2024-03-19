719458dd658bdeebacd7495af95305fe705ef3b3
esther Mwangi-Häber <esther.wangechi.mwangi@gmail.com>
2024-03-19 21:12:14 +0100

    second committs

--- app.js
+++ app.js
@@ -12,22 +12,30 @@ import {
   routeNotFound,
 } from "./middlewares/errorHandler.js";
 import { getProductById } from "./controllers/productsController.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const { PORT, DB_URI } = process.env;
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = path.dirname(__fileName);
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://ecommerce-hautecourture.onrender.com"],
+  origin: ["http://localhost:5173", "https://ecommerce-hautecourture.onrender.com","https://ecommerce-fullstack-luf0.onrender.com"],
   credentials: true,
 };
 
 app
   .use(cors(corsOptions))
   .use(express.json())
+  .use(express.static(path.join(__dirName, "/dist")))
   .use(cookieParser())
   .use("/users", usersRouter)
   .use("/products", productsRouter)
   .use("/carts", cartRouter)
+  .get("/*", (req, res, next) => {
+    res.sendFile(path.join(__dirName, "/dist", "index.html"));
+  })
   .use(routeNotFound)
   .use(globalErrorHandler)
   .listen(PORT, () => console.log(`Server is running on port ${PORT}`));
